1d618ad0d8540b74c1d2b405d4169279b96a47ac
rafkat <rafgaleev3@gmail.com>
2025-02-24 20:04:26 +0300

    added ResNet

---
+++ models/ResNet34.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super(ResidualBlock, self).__init__()
+        if downsample:
+            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2)
+            self.bn_sc = nn.BatchNorm2d(out_channels)
+        else:
+            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if x.size() != out.size():
+            shortcut = self.bn_sc(self.shortcut(x))
+            return self.relu(out + shortcut)
+        return self.relu(out + x)
+
+
+class ResNet34(nn.Module):
+    def __init__(self):
+        super(ResNet34, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.res_layer1 = nn.Sequential(
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+        )
+
+        self.res_layer2 = nn.Sequential(
+            ResidualBlock(64, 128, downsample=True),
+            ResidualBlock(128, 128),
+            ResidualBlock(128, 128),
+            ResidualBlock(128, 128),
+        )
+
+        self.res_layer3 = nn.Sequential(
+            ResidualBlock(128, 256, downsample=True),
+            ResidualBlock(256, 256),
+            ResidualBlock(256, 256),
+            ResidualBlock(256, 256),
+            ResidualBlock(256, 256),
+            ResidualBlock(256, 256),
+        )
+
+        self.res_layer4 = nn.Sequential(
+            ResidualBlock(256, 512, downsample=True),
+            ResidualBlock(512, 512),
+            ResidualBlock(512, 512),
+        )
+
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512, 1000)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.res_layer1(x)
+        x = self.res_layer2(x)
+        x = self.res_layer3(x)
+        x = self.res_layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
---
+++ models/ResNet50.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+
+
+class BottleneckResidualBlock(nn.Module):
+    def __init__(self, in_channels, intermediate, out_channels, downsample=False):
+        super(BottleneckResidualBlock, self).__init__()
+        if not downsample:
+            self.conv1 = nn.Conv2d(in_channels, intermediate, kernel_size=1, stride=1)
+            self.shortcut = nn.Conv2d(intermediate, out_channels, kernel_size=1, stride=1)
+        else:
+            self.conv1 = nn.Conv2d(in_channels, intermediate, kernel_size=1, stride=2)
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2)
+
+        self.bn_sc = nn.BatchNorm2d(out_channels)
+
+        self.bn1 = nn.BatchNorm2d(intermediate)
+        self.conv2 = nn.Conv2d(intermediate, intermediate, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(intermediate)
+        self.conv3 = nn.Conv2d(intermediate, out_channels, kernel_size=1, stride=1)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        if x.size() != out.size():
+            shortcut = self.bn_sc(self.shortcut(x))
+            return self.relu(out + shortcut)
+        return self.relu(out + x)
+
+
+class ResNet50(nn.Module):
+    def __init__(self):
+        super(ResNet50, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.res_layer1 = nn.Sequential(
+            BottleneckResidualBlock(64, 64, 256),
+            BottleneckResidualBlock(256, 64, 256),
+            BottleneckResidualBlock(256, 64, 256),
+        )
+
+        self.res_layer2 = nn.Sequential(
+            BottleneckResidualBlock(256, 128, 512, downsample=True),
+            BottleneckResidualBlock(512, 128, 512),
+            BottleneckResidualBlock(512, 128, 512),
+            BottleneckResidualBlock(512, 128, 512),
+        )
+
+        self.res_layer3 = nn.Sequential(
+            BottleneckResidualBlock(512, 256, 1024, downsample=True),
+            BottleneckResidualBlock(1024, 256, 1024),
+            BottleneckResidualBlock(1024, 256, 1024),
+            BottleneckResidualBlock(1024, 256, 1024),
+            BottleneckResidualBlock(1024, 256, 1024),
+            BottleneckResidualBlock(1024, 256, 1024),
+        )
+
+        self.res_layer4 = nn.Sequential(
+            BottleneckResidualBlock(1024, 512, 2048, downsample=True),
+            BottleneckResidualBlock(2048, 512, 2048),
+            BottleneckResidualBlock(2048, 512, 2048),
+        )
+
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(2048, 1000)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.res_layer1(x)
+        x = self.res_layer2(x)
+        x = self.res_layer3(x)
+        x = self.res_layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
