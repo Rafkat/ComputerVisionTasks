2ba3cb4c7b7986f15e41055e66d6fc13ce721f14
rafkat <rafgaleev3@gmail.com>
2025-03-28 15:57:25 +0300

    changed weight decay

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ for model_config in models_config.values():
     training = Cifar10Training(model.to(dvc))
     train_dataloader, val_dataloader = training.load_cifar_data(img_size=model_config['img_size'],
                                                                 batch_size=model_config['batch_size'])
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=1e-4)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=1e-1)
     training.train(train_dataloader, val_dataloader, optimizer,
                    loss_func=nn.CrossEntropyLoss(), epochs=50, device=dvc, postfix='early_stop_sgd')
