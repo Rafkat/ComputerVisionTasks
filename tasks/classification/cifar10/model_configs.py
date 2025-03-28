ce7ad58cb95f6c811ae5fa92c6ba8fa29400925a
rafkat <rafgaleev3@gmail.com>
2025-03-28 15:59:41 +0300

    fixed paths

--- tasks/classification/cifar10/model_configs.py
+++ tasks/classification/cifar10/model_configs.py
@@ -1,73 +1,75 @@
+import models.classification_convolution as conv
+
 VGG16_CONFIG = {
     'batch_size': 128,
     'img_size': 32,
-    'model': models.convolution_models.VGG16.VGG16(input_size=32, nb_classes=10),
+    'model': conv.VGG16.VGG16(input_size=32, nb_classes=10),
 }
 
 ALEXNET_CONFIG = {
     'batch_size': 128,
     'img_size': 32,
-    'model': models.convolution_models.AlexNet.AlexNet(input_size=32, nb_classes=10),
+    'model': conv.AlexNet.AlexNet(input_size=32, nb_classes=10),
 }
 
 CONVNEXT_CONFIG = {
     'batch_size': 128,
     'img_size': 32,
-    'model': models.convolution_models.ConvNext.ConvNext(nb_classes=10)
+    'model': conv.ConvNext.ConvNext(nb_classes=10)
 }
 
 GOOGLENET_CONFIG = {
     'batch_size': 128,
     'img_size': 224,
-    'model': models.convolution_models.GoogLeNet.GoogLeNet(nb_classes=10),
+    'model': conv.GoogLeNet.GoogLeNet(nb_classes=10),
 }
 
 LENET_CONFIG = {
     'batch_size': 128,
     'img_size': 224,
-    'model': models.convolution_models.LeNet5.LeNet5(in_channels=3, nb_classes=10),
+    'model': conv.LeNet5.LeNet5(in_channels=3, nb_classes=10),
 }
 
 MOBILENETV1_CONFIG = {
     'batch_size': 128,
     'img_size': 224,
-    'model': models.convolution_models.MobileNet_v1.MobileNetV1(nb_classes=10),
+    'model': conv.MobileNet_v1.MobileNetV1(nb_classes=10),
 }
 
 MOBILENETV2_CONFIG = {
     'batch_size': 128,
     'img_size': 224,
-    'model': models.convolution_models.MobileNet_v2.MobileNetV2(nb_classes=10),
+    'model': conv.MobileNet_v2.MobileNetV2(nb_classes=10),
 }
 
 RESNET34_CONFIG = {
     'batch_size': 128,
     'img_size': 224,
-    'model': models.convolution_models.ResNet34.ResNet34(nb_classes=10),
+    'model': conv.ResNet34.ResNet34(nb_classes=10),
 }
 
 RESNET50_CONFIG = {
     'batch_size': 64,
     'img_size': 224,
-    'model': models.convolution_models.ResNet50.ResNet50(nb_classes=10),
+    'model': conv.ResNet50.ResNet50(nb_classes=10),
 }
 
 RESNEXT_CONFIG = {
     'batch_size': 16,
     'img_size': 128,
-    'model': models.convolution_models.ResNext.ResNeXt(nb_classes=10),
+    'model': conv.ResNext.ResNeXt(nb_classes=10),
 }
 
 XCEPTION_CONFIG = {
     'batch_size': 64,
     'img_size': 224,
-    'model': models.convolution_models.Xception.Xception(nb_classes=10),
+    'model': conv.Xception.Xception(nb_classes=10),
 }
 
 DENSENET_CONFIG = {
     'batch_size': 128,
     'img_size': 224,
-    'model': models.convolution_models.DenseNet.DenseNet(nb_classes=10),
+    'model': conv.DenseNet.DenseNet(nb_classes=10),
 }
 
 models_config = {
