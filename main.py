a5d5842c53e7a514fffa771924e4d7a5b8dd6f0e
rafkat <rafgaleev3@gmail.com>
2025-04-02 19:16:39 +0300

    removed momentum

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ for model_config in models_config.values():
     training = Cifar10Training(model.to(dvc))
     train_dataloader, val_dataloader = training.load_cifar_data(img_size=model_config['img_size'],
                                                                 batch_size=model_config['batch_size'])
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
     training.train(train_dataloader, val_dataloader, optimizer,
-                   loss_func=nn.CrossEntropyLoss(), epochs=50, device=dvc, postfix='early_stop_sgd')
+                   loss_func=nn.CrossEntropyLoss(), epochs=50, device=dvc, postfix='early_stop_sgd_no_momentum')
