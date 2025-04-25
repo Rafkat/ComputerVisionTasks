170b21ed92202d8a837e65bb946dc0886ff98781
rafkat <rafgaleev3@gmail.com>
2025-04-25 22:11:41 +0300

    added references

--- models/detection_convolution/__init__.py
+++ models/detection_convolution/__init__.py
@@ -1,2 +1,5 @@
 # rcnn model https://github.com/HoganHPH/RCNN-Object-Detection-From-Scratch
 
+# faster rcnn model https://github.com/wingedrasengan927/pytorch-tutorials/tree/master/Object%20Detection
+
+# ssd model https://github.com/anhtuan85/Pytorch-SSD-from-scratch/tree/master
